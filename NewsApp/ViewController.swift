6c01a5c6ffc9a329d9a6c786c095788989889559
Msbarari <msbarari@gmail.com>
2022-09-18 06:19:31 +0300

    create the project

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by DG on 18/09/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
